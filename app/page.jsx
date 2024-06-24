bb8672143fb5f07d260a1125e0126a8f08a189e1
Kaushik Saha <116095138+Kaushik1766@users.noreply.github.com>
2024-06-24 16:50:21 +0530

    input position changed

--- app/page.jsx
+++ app/page.jsx
@@ -11,15 +11,6 @@ export default function Home() {
   const [refresher, setRefresher] = useState(0)
   return (
     <>
-      <div className="w-full h-screen">
-        <Canvas key={refresher} camera={{ position: [0, 0, 5], fov: 75 }}>
-          <Environment preset="apartment" />
-          <OrbitControls enableZoom={true} />
-          <Suspense fallback={null}>
-            <Model file={file} />
-          </Suspense>
-        </Canvas>
-      </div >
       <form onSubmit={(e) => {
         e.preventDefault()
         const data = e.target.model.files[0]
@@ -31,6 +22,16 @@ export default function Home() {
         <input ref={inputRef} type="file" name="model" id="model" />
         <button type="submit">submit</button>
       </form>
+      <div className="w-full h-[90vh]">
+        <Canvas shadows={true} key={refresher} camera={{ position: [0, 0, 5], fov: 75 }}>
+          <Environment preset="sunset" />
+          <OrbitControls enableZoom={true} />
+          <Suspense fallback={null}>
+            <Model file={file} />
+          </Suspense>
+        </Canvas>
+      </div >
+
     </>
   );
 }
